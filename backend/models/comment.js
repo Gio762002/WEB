0851ab11b316f1cf94733bc70e142dbc14c4d683
Gio762002 <guo6437@outlook.com>
2024-03-14 22:11:43 +0100

    backend connected to db via 3010 ok, db test for testing

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -8,4 +8,4 @@ const TestSchema = new Schema({
     name: String,
 }, { timestamps: true, collection: 'test'});
 const test = mongoose.model('test', TestSchema);
-module.exports = test
+module.exports = test 
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const testdb = require("./models/comment");
-
+ 
 // and create our instances
 const app = express();
 const router = express.Router();
