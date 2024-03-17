13d5698515b7a58dca08b1e7119b851c24f72525
Gio762002 <guo6437@outlook.com>
2024-03-17 21:15:53 +0100

    model for authen

---
+++ backend/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
