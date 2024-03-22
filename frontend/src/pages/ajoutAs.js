31e0c6ef84404ea2bda312d47fd391d2cc0f0884
kuch19 <96070632+kuch19@users.noreply.github.com>
2024-03-22 15:22:14 +0100

    Update ajoutAs.js

--- frontend/src/pages/ajoutAs.js
+++ frontend/src/pages/ajoutAs.js
@@ -1,261 +1,98 @@
-import React, { useMemo, useState } from 'react';
-import { Button, Grid, AppBar, Toolbar, Typography, Select, MenuItem, Popper, Paper, ClickAwayListener } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
 import routerImage from '../routeurss.png'; // Importez votre image de routeur
 import '../App.css';
 
 function AjoutAs() {
-  const [projects, setProjects] = useState([]); // Liste des projets
-  const [selectedProject, setSelectedProject] = useState(''); // Projet sélectionné
-  const [asData, setAsData] = useState([]); // Données de l'AS pour le projet actuel
-  const [selectedRouter, setSelectedRouter] = useState(null); // Routeur sélectionné
-  const [routerInterfaces, setRouterInterfaces] = useState([]); // Interfaces du routeur sélectionné
-  const [anchorEl, setAnchorEl] = useState(null); // Élément d'ancrage pour le Popper
-  const [open, setOpen] = useState(false); // État d'ouverture du Popper
-  const [otherRouters, setOtherRouters] = useState([]); // Autres routeurs
-  const [routerCount, setRouterCount] = useState(1); // Compteur de routeurs
-  const [asCount, setAsCount] = useState(0); // Compteur d'AS
+  const [routers, setRouters] = useState([]); // Liste des routeurs avec leurs positions
+  const [activeRouterIndex, setActiveRouterIndex] = useState(null); // Indice du routeur actuellement sélectionné pour le déplacement
+  const [connections, setConnections] = useState([]); // Liste des connexions entre les routeurs
 
-  const RouterCount = () => {
-    setRouterCount(routerCount + 1);
-  };
-  const AsCount = () => {
-    setAsCount(prevData => prevData + 1);
-  };
-
-  // Créer une nouvelle AS dans le projet actuel
-  const createNewAS = () => {
-    AsCount();
-    const newAS = {
-      id: asCount,
-      asNumber: asCount+1,
-      igp : '',
-      routers : [],
-      links: [],
-      buttons: [{id:0, type: "As"},{id:100, type: "add"}],
-    }
-    
-    setAsData(prevData => [...prevData, newAS]);
-  };
-
-  // Ajouter un routeur dans l'AS
-  const addRouterButton = (containerIndex,) => {
-    // createNewRouter(containerIndex);
-    setAsData(prevData => {
-      const updatedData = [...prevData];
-      const asDataItem = updatedData[containerIndex];
-      const addButton = asDataItem.buttons.pop();
-      asDataItem.buttons.push({
-        type: "routeur",
-        id: routerCount,
-        name: `Routeur ${routerCount}`,
-        position: containerIndex,
-        protocol: '',
-        loopback: '',
-        interfaces:  {1:'interfaceEthernet0/0', 2:'interfaceEthernet0/1', 3:'interfaceSerial0/0'},
-        interfacestatus: {1:'down', 2:'down', 3:'down'},
-        interfaceip: {1:'', 2:'', 3:''},
-        });
-      asDataItem.buttons.push(addButton);
-      RouterCount();
-      return updatedData;
-    });
-  };
-
-  // Gérer le clic sur le bouton du nom d'un routeur
-  const handleRouterNameClick = (routerId) => {
-    // Vous pouvez implémenter ici la logique pour afficher les informations sur les interfaces du routeur
-    console.log("Informations sur les interfaces du routeur:", routerInterfaces);
-  };
-
-    // Afficher le nom du routeur sélectionné
-  const handleRouterButtonClick = (event, routerId) => {
-    setSelectedRouter(routerId);
-    setAnchorEl(event.currentTarget);
-    setOpen(true);
-    const currentContainer = asData.find(container => container.buttons.some(button => button.id === routerId));
-    const otherRoutersNames = currentContainer.buttons
-      .filter(button => button.type === "routeur" && button.id !== routerId)
-      .map(button => {
-        const otherRouterContainer = asData.find(container => container.buttons.some(btn => btn.id === button.id));
-        return { id: button.id, name: `Routeur ${button.id} (${otherRouterContainer.name})` };
-      });
-    setOtherRouters(otherRoutersNames);
-
-    // Simuler la récupération des interfaces du routeur sélectionné (remplacer par la logique appropriée)
-    const interfaces = ['interfaceEthernet0/0', 'interfaceEthernet0/1', 'interfaceSerial0/0'];
-    setRouterInterfaces(interfaces);
-  };
-
-  const henderButtons = (container) => {
-    console.log('called henderButtons');
-    return container.buttons.map(button => {
-        let buttonElement;
-        if (button.type === "As") {
-          buttonElement = (
-            <Button 
-              variant="contained" 
-              color="primary"
-              size="small" // Taille du bouton réduite
-            >
-              {`AS ${container.asNumber}`}
-            </Button>
-          );
-        } else if (button.type === "add") {
-          buttonElement = (
-            <Button 
-              variant="outlined" 
-              color="primary"
-              onClick={() => addRouterButton(container.id)}
-              size="small" // Taille du bouton réduite
-            >
-              {`+`}
-            </Button>
-          );
-        } else if (button.type === "routeur") {
-          buttonElement = (
-            <Button 
-              variant="contained" 
-              color="secondary"
-              onClick={(event) => handleRouterButtonClick(event, button.id)}
-              style={{ position: 'relative' }}
-              size="small" // Taille du bouton réduite
-            >
-              <img src={routerImage} alt={` ${button.id}`} className="router-image" />
-              <span style={{ position: 'absolute', top: '5px', right: '4px' }}>{button.id}</span>
-            </Button>
-          );
-        }
-      return (
-        <div key={button.id}>
-          {buttonElement}
-        </div>
-      );
-    })
-  };
-
-  // Créer un nouveau projet
-  const createNewProject = () => {
-    const newProject = {
-      id: projects.length + 1,
-      name: `Projet ${projects.length + 1}`,
-      asData: [] // Données de l'AS pour ce projet
-    };
-    setProjects(prevProjects => [...prevProjects, newProject]);
-    setSelectedProject(newProject.name); // Sélectionner le nouveau projet
-    setAsData([]); // Réinitialiser les données de l'AS
-  };
-
-  // Effacer un projet
-  const clearProject = () => {
-    setProjects([]);
-    setSelectedProject('');
-    setAsData([]);
-  };
-
-  // Enregistrer le projet actuel
-  const saveProject = () => {
-    const projectIndex = projects.findIndex(project => project.name === selectedProject);
-    if (projectIndex !== -1) {
-      const updatedProjects = [...projects];
-      updatedProjects[projectIndex].asData = asData;
-      setProjects(updatedProjects);
-      alert('Projet enregistré avec succès!');
+  // Fonction pour ajouter un nouveau routeur
+  const addRouter = (event) => {
+    if (event.target.tagName === 'BUTTON') { // Vérifier si le clic provient du bouton "Ajouter un routeur"
+      const newRouter = {
+        id: routers.length,
+        x: event.clientX - 25, // Positionner le centre de l'image à l'emplacement du clic
+        y: event.clientY - 25,
+      };
+      setRouters(prevRouters => [...prevRouters, newRouter]);
+      setActiveRouterIndex(routers.length); // Sélectionner le nouveau routeur pour le déplacement immédiat
     }
   };
 
-  // Gérer le changement de projet sélectionné
-  const handleProjectChange = (event) => {
-    setSelectedProject(event.target.value);
-    const project = projects.find(proj => proj.name === event.target.value);
-    if (project) {
-      setAsData(project.asData);
-    } else {
-      setAsData([]);
+  // Fonction pour mettre à jour la position d'un routeur lors du déplacement
+  const updateRouterPosition = (event) => {
+    if (activeRouterIndex !== null) {
+      const newRouters = [...routers];
+      newRouters[activeRouterIndex] = {
+        ...newRouters[activeRouterIndex],
+        x: event.clientX - 25, // Positionner le centre de l'image à l'emplacement du curseur
+        y: event.clientY - 25,
+      };
+      setRouters(newRouters);
     }
   };
 
-  // Fermer le Popper
-  const handleClose = () => {
-    setOpen(false);
+  // Fonction pour sélectionner un routeur pour le déplacement
+  const selectRouter = (index) => {
+    setActiveRouterIndex(index);
   };
 
+  // Fonction pour désélectionner le routeur actif
+  const deselectRouter = () => {
+    setActiveRouterIndex(null);
+  };
+
+  // Fonction pour relier deux routeurs
+  const connectRouters = (event) => {
+    const selectedRouterIndex = parseInt(event.target.dataset.index);
+    if (!isNaN(selectedRouterIndex)) {
+      if (activeRouterIndex !== null && activeRouterIndex !== selectedRouterIndex) {
+        const newConnections = [...connections, { from: activeRouterIndex, to: selectedRouterIndex }];
+        setConnections(newConnections);
+      }
+    }
+  };
 
   return (
-    <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-            <Select
-              value={selectedProject}
-              onChange={handleProjectChange}
-              displayEmpty
-              inputProps={{ 'aria-label': 'Projet' }}
-            >
-              <MenuItem value="" disabled>
-                Liste des projets
-              </MenuItem>
-              {projects.map(project => (
-                <MenuItem key={project.id} value={project.name}>{project.name}</MenuItem>
-              ))}
-            </Select>
-          </Typography>
-          <Button color="inherit" onClick={createNewProject}>Nouveau projet</Button>
-          <Button color="inherit" onClick={saveProject}>Enregistrer projet</Button>
-          <Button color="inherit" onClick={clearProject}>Effacer projet</Button>
-        </Toolbar>
-      </AppBar>
+    <div className="App" onMouseMove={updateRouterPosition} onMouseDown={addRouter}>
       <h1>Vous pouvez créer la topologie du réseau</h1>
       <Grid container spacing={2}>
         <Grid item>
-          <Button variant="contained" color="primary" onClick={createNewAS}>
-            Créer une nouvelle AS
-          </Button>
+          <button>Ajouter un routeur</button>
+        </Grid>
+        <Grid item>
+          <button onClick={connectRouters}>Relier</button>
         </Grid>
       </Grid>
       <div className="as-wrapper">
-        {asData.map(container => (
-          <div key={container.id} className="as-container">
-            <div className="rectangle">
-              {
-              henderButtons(container)
-              }
-            </div>
-          </div>
+        {connections.map((connection, index) => (
+          <svg key={index} style={{ position: 'absolute', zIndex: -1 }}>
+            <line
+              x1={routers[connection.from].x + 25}
+              y1={routers[connection.from].y + 25}
+              x2={routers[connection.to].x + 25}
+              y2={routers[connection.to].y + 25}
+              style={{ stroke: 'black', strokeWidth: 2 }}
+            />
+          </svg>
         ))}
-        <Popper open={open} anchorEl={anchorEl} placement='top'>
-          <ClickAwayListener onClickAway={handleClose}>
-            <Paper>
-              <Typography variant="body1" style={{ padding: '8px' }}>
-                Routeur sélectionné : {selectedRouter}
-              </Typography>
-              <Typography variant="body1" style={{ padding: '8px' }}>
-                Informations sur les interfaces du routeur :
-              </Typography>
-              <ul>
-                {routerInterfaces.map((interfaceName, index) => (
-                  <li key={index}>{interfaceName}</li>
-                ))}
-              </ul>
-              <Typography variant="body1" style={{ padding: '8px' }}>
-                Tu veux le relier avec quel routeur ?
-              </Typography>
-              {otherRouters.map((router, index) => (
-                <Button 
-                  key={index} 
-                  variant="contained" 
-                  style={{ backgroundColor: '#00ff00', margin: '4px' }} 
-                  size="small"
-                  onClick={() => handleRouterNameClick(router.id)} // Gérer le clic sur le nom du routeur
-                >
-                  {router.name}
-                </Button>
-              ))}
-            </Paper>
-          </ClickAwayListener>
-        </Popper>
+        {routers.map((router, index) => (
+          <img
+            key={index}
+            src={routerImage}
+            alt={`Routeur ${index}`}
+            className="router-image"
+            style={{ position: 'absolute', left: router.x, top: router.y, cursor: 'move' }}
+            onMouseDown={() => selectRouter(index)}
+            onMouseUp={deselectRouter}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
 export default AjoutAs;
+
