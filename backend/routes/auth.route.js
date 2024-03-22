2d07f2d36cc7ac9ec38b0eb375089fa9a3a77196
Yichao Mao <108553589+MickealMAO@users.noreply.github.com>
2024-03-22 17:47:49 +0100

    Delete backend directory

--- backend/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup, signin } = require("../controllers/auth.controller.js");
-
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/signin", signin);
-
-module.exports = router;
--- backend/utils/error.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (statusCode, message) => {
-  const error = new Error(message);
-  error.statusCode = statusCode;
-  // error.message = message;
-  return error;
-};
-
-module.exports = errorHandler;
